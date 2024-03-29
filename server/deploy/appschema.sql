55aebb283ef6dd8e7ec92e4e64df8e39b5847126
Jacob Smith <jsmithdenverdev@gmail.com>
2024-03-29 11:41:52 -0600

    app schema updates

--- server/deploy/appschema.sql
+++ server/deploy/appschema.sql
@@ -1,77 +1,76 @@
 -- Deploy pager:appschema to pg
-
 BEGIN;
 
--- Enable UUID generation
+---------------------------
+-- EXTENSIONS
+---------------------------
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 ---------------------------
 -- AGENCIES
 ---------------------------
-
 CREATE TABLE IF NOT EXISTS agency_status (
   "status" varchar(256) NOT NULL,
   CONSTRAINT agency_status_pk PRIMARY KEY ("status")
 );
 
-INSERT INTO agency_status ("status") VALUES 
-  ('PENDING'),
+INSERT INTO agency_status ("status")
+VALUES ('PENDING'),
   ('ACTIVE'),
   ('INACTIVE');
 
-
 CREATE TABLE IF NOT EXISTS agencies (
-	id uuid DEFAULT gen_random_uuid() NOT NULL,
-	"name" varchar(256) NOT NULL,
+  id uuid DEFAULT gen_random_uuid() NOT NULL,
+  "name" varchar(256) NOT NULL,
   "status" varchar(256) NOT NULL REFERENCES agency_status("status"),
-	created timestamptz DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
-	created_by varchar(256) NOT NULL,
-	modified timestamp DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
-	modified_by varchar(256) NOT NULL,
-	CONSTRAINT agencies_pk PRIMARY KEY (id)
+  created timestamptz DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
+  created_by varchar(256) NOT NULL,
+  modified timestamp DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
+  modified_by varchar(256) NOT NULL,
+  CONSTRAINT agencies_pk PRIMARY KEY (id)
 );
 
 ---------------------------
 -- USERS
 ---------------------------
-
 CREATE TABLE IF NOT EXISTS user_status (
   "status" varchar(256) NOT NULL,
   CONSTRAINT user_status_pk PRIMARY KEY ("status")
 );
 
-INSERT INTO user_status ("status") VALUES 
-  ('PENDING'),
+INSERT INTO user_status ("status")
+VALUES ('PENDING'),
   ('ACTIVE'),
   ('INACTIVE');
 
 CREATE TABLE IF NOT EXISTS users (
   id uuid DEFAULT gen_random_uuid() NOT NULL,
-  "idp_id" varchar(256) NOT NULL,
+  email varchar(256) NOT NULL UNIQUE,
+  "idp_id" varchar(256) NOT NULL UNIQUE,
   "status" varchar(256) NOT NULL REFERENCES user_status("status"),
   created timestamptz DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
-	created_by varchar(256) NOT NULL,
-	modified timestamp DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
-	modified_by varchar(256) NOT NULL,
-	CONSTRAINT users_pk PRIMARY KEY (id)
+  created_by varchar(256) NOT NULL,
+  modified timestamp DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
+  modified_by varchar(256) NOT NULL,
+  CONSTRAINT users_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS user_agencies(
   user_id uuid NOT NULL REFERENCES users(id),
-  agency_id uuid NOT NULL REFERENCES agencies(id)
+  agency_id uuid NOT NULL REFERENCES agencies(id),
+  CONSTRAINT user_agencies_unique_user_id_agency_id UNIQUE (user_id, agency_id)
 );
 
 ---------------------------
 -- DEVICES
 ---------------------------
-
 CREATE TABLE IF NOT EXISTS device_status (
   "status" varchar(256) NOT NULL,
   CONSTRAINT device_status_pk PRIMARY KEY ("status")
 );
 
-INSERT INTO device_status ("status") VALUES 
-  ('PENDING'),
+INSERT INTO device_status ("status")
+VALUES ('PENDING'),
   ('ACTIVE'),
   ('INACTIVE');
 
@@ -82,23 +81,22 @@ CREATE TABLE IF NOT EXISTS devices (
   user_id uuid NOT NULL REFERENCES users(id),
   code varchar(256) NOT NULL,
   created timestamptz DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
-	created_by varchar(256) NOT NULL,
-	modified timestamp DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
-	modified_by varchar(256) NOT NULL,
-	CONSTRAINT devices_pk PRIMARY KEY (id)
+  created_by varchar(256) NOT NULL,
+  modified timestamp DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
+  modified_by varchar(256) NOT NULL,
+  CONSTRAINT devices_pk PRIMARY KEY (id)
 );
 
 ---------------------------
 -- PAGES
 ---------------------------
-
 CREATE TABLE IF NOT EXISTS page_delivery_status (
   "status" varchar(256) NOT NULL,
   CONSTRAINT page_delivery_status_pk PRIMARY KEY ("status")
 );
 
-INSERT INTO page_delivery_status ("status") VALUES 
-  ('PENDING'),
+INSERT INTO page_delivery_status ("status")
+VALUES ('PENDING'),
   ('DELIVERING'),
   ('DELIVERED'),
   ('DELIVERY_FAILED');
@@ -110,10 +108,10 @@ CREATE TABLE IF NOT EXISTS pages (
   -- large
   "content" varchar(65536) NULL,
   created timestamptz DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
-	created_by varchar(256) NOT NULL,
-	modified timestamp DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
-	modified_by varchar(256) NOT NULL,
-	CONSTRAINT pages_pk PRIMARY KEY (id)
+  created_by varchar(256) NOT NULL,
+  modified timestamp DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
+  modified_by varchar(256) NOT NULL,
+  CONSTRAINT pages_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS page_deliveries(
@@ -124,9 +122,11 @@ CREATE TABLE IF NOT EXISTS page_deliveries(
   created timestamptz DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
   -- Not sure that created_by and modified_by make sense, this will always be
   -- 'system'. But it doesn't hurt to have the data.
-	created_by varchar(256) NOT NULL,
-	modified timestamp DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
-	modified_by varchar(256) NOT NULL,
+  created_by varchar(256) NOT NULL,
+  modified timestamp DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
+  modified_by varchar(256) NOT NULL,
+  CONSTRAINT page_deliveries_pk PRIMARY KEY (id),
+  CONSTRAINT page_deliveries_unique_page_id_device_id UNIQUE (page_id, device_id)
 );
 
 COMMIT;
