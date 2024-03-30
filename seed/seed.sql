8a9cebc4b3cf604243fd33836b7710a68e830d64
Jacob Smith <jsmithdenverdev@gmail.com>
2024-03-29 18:08:58 -0600

    add second agency to seed

--- seed/seed.sql
+++ seed/seed.sql
@@ -22,7 +22,14 @@ VALUES (
 INSERT INTO agencies (id, "name", "status", created_by, modified_by)
 VALUES (
   '8d282649-708b-4523-ab2a-122bd8739bd1',
-  'Sample Agency',
+  'Sample SAR Agency',
+  'ACTIVE',
+  'auth0|6606e16332edd5a3975a6e9f',
+  'auth0|6606e16332edd5a3975a6e9f'
+),
+(
+  '78c95398-3b3d-4e82-a648-0787af58a945',
+  'Sample EMS Agency',
   'ACTIVE',
   'auth0|6606e16332edd5a3975a6e9f',
   'auth0|6606e16332edd5a3975a6e9f'
@@ -32,7 +39,9 @@ ON CONFLICT DO NOTHING;
 -- Create user -> agency relation
 INSERT INTO user_agencies (user_id, agency_id)
 VALUES (
+  -- agency-admin@pager.com
   'ec3e634d-e966-44f4-9583-90e3c41fa932',
+  -- Sample SAR Agency
   '8d282649-708b-4523-ab2a-122bd8739bd1'
 )
 ON CONFLICT DO NOTHING;
