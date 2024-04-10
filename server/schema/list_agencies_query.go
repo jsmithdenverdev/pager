689355e45fd4acd44d2a9e85ae268f344ace188e
Jacob Smith <jsmithdenverdev@gmail.com>
2024-04-10 16:07:30 -0600

    add sort support to agencies query

--- server/schema/list_agencies_query.go
+++ server/schema/list_agencies_query.go
@@ -5,27 +5,69 @@ import (
 	"github.com/jsmithdenverdev/pager/service"
 )
 
+// agenciesSortType is a graphql enum representing the sort order for agencies.
+var agenciesSortType = graphql.NewEnum(graphql.EnumConfig{
+	Name: "AgenciesSort",
+	Values: graphql.EnumValueConfigMap{
+		"CREATED_ASC": &graphql.EnumValueConfig{
+			Value: service.AgenciesOrderCreatedAsc,
+		},
+		"CREATED_DESC": &graphql.EnumValueConfig{
+			Value: service.AgenciesOrderCreatedDesc,
+		},
+		"MODIFIED_ASC": &graphql.EnumValueConfig{
+			Value: service.AgenciesOrderModifiedAsc,
+		},
+		"MODIFIED_DESC": &graphql.EnumValueConfig{
+			Value: service.AgenciesOrderModifiedDesc,
+		},
+		"NAME_ASC": &graphql.EnumValueConfig{
+			Value: service.AgenciesOrderNameAsc,
+		},
+		"NAME_DESC": &graphql.EnumValueConfig{
+			Value: service.AgenciesOrderNameDesc,
+		},
+	},
+})
+
 // listAgenciesQuery is the field definition for the agencies query.
 var listAgenciesQuery = &graphql.Field{
 	Name: "agencies",
 	Type: graphql.NewList(agencyType),
 	Args: graphql.FieldConfigArgument{
 		"first": &graphql.ArgumentConfig{
-			Type: graphql.Int,
+			Type:         graphql.Int,
+			DefaultValue: 10,
 		},
 		"after": &graphql.ArgumentConfig{
 			Type: graphql.String,
 		},
 		"sort": &graphql.ArgumentConfig{
-			Type: graphql.String,
+			Type:         agenciesSortType,
+			DefaultValue: service.AgenciesOrderCreatedAsc,
 		},
 	},
 	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 		svc := p.Context.Value(service.ContextKeyAgencyService).(*service.AgencyService)
-		return svc.List(service.AgenciesPagination{
-			First: p.Args["first"].(int),
-			After: p.Args["after"].(string),
-			Order: service.AgenciesOrderCreatedAsc,
-		})
+		var (
+			argsFirst  = p.Args["first"]
+			argsAfter  = p.Args["after"]
+			argsOrder  = p.Args["sort"]
+			pagination service.AgenciesPagination
+		)
+
+		if argsFirst != nil {
+			pagination.First = argsFirst.(int)
+		}
+
+		if argsAfter != nil {
+			pagination.After = argsAfter.(string)
+		}
+
+		if argsOrder != nil {
+			pagination.Order = argsOrder.(service.AgenciesOrder)
+		}
+
+		return svc.List(pagination)
 	},
 }
