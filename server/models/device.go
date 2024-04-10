be3b2c905bc44e13100f50aa42a0d31f5a1cd73b
Jacob Smith <jsmithdenverdev@gmail.com>
2024-04-10 16:07:02 -0600

    scaffold out device support

---
+++ server/models/device.go
@@ -0,0 +1,24 @@
+package models
+
+type DeviceStatus string
+
+const (
+	DeviceStatusPending  DeviceStatus = "PENDING"
+	DeviceStatusActive   DeviceStatus = "ACTIVE"
+	DeviceStatusInactive DeviceStatus = "INACTIVE"
+)
+
+// Device is a core entity of pager.
+//
+// A device belongs to a User, who may be registered with one or more Agencies.
+// Notifications in pager are pushed to Devices. Currently, push notifications
+// are what will be delivered, and we track the push endpoint for that device
+// on the row.
+type Device struct {
+	Auditable
+	ID       string       `json:"id" db:"id"`
+	Status   DeviceStatus `json:"status" db:"status"`
+	Endpoint string       `json:"endpoint" db:"endpoint"`
+	UserID   string       `json:"userId" db:"user_id"`
+	Code     string       `json:"code" db:"code"`
+}
---
+++ server/schema/device_type.go
@@ -0,0 +1,68 @@
+package schema
+
+import (
+	"github.com/graphql-go/graphql"
+	"github.com/jsmithdenverdev/pager/models"
+)
+
+// deviceStatusType  is a graphql enum representing the status of a device.
+var deviceStatusType = graphql.NewEnum(graphql.EnumConfig{
+	Name: "DeviceStatus",
+	Values: graphql.EnumValueConfigMap{
+		"PENDING": &graphql.EnumValueConfig{
+			Value: models.DeviceStatusPending,
+		},
+		"ACTIVE": &graphql.EnumValueConfig{
+			Value: models.DeviceStatusActive,
+		},
+		"INACTIVE": &graphql.EnumValueConfig{
+			Value: models.DeviceStatusInactive,
+		},
+	},
+})
+
+// deviceType is the object definition for a device.
+var deviceType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Device",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.ID,
+		},
+		"status": &graphql.Field{
+			Type: deviceStatusType,
+		},
+		"endpoint": &graphql.Field{
+			Type: graphql.String,
+		},
+		"userId": &graphql.Field{
+			Type: graphql.String,
+		},
+		"code": &graphql.Field{
+			Type: graphql.String,
+		},
+		"created": &graphql.Field{
+			Type: graphql.DateTime,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return p.Source.(models.Device).Created, nil
+			},
+		},
+		"createdBy": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return p.Source.(models.Device).CreatedBy, nil
+			},
+		},
+		"modified": &graphql.Field{
+			Type: graphql.DateTime,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return p.Source.(models.Device).Modified, nil
+			},
+		},
+		"modifiedBy": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return p.Source.(models.Device).ModifiedBy, nil
+			},
+		},
+	},
+})
