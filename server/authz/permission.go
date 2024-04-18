c6ea706226311919ef82f8a99cce49177f18f6e4
Jacob Smith <jsmithdenverdev@gmail.com>
2024-04-18 07:58:26 -0600

    collapse permisssions and add new permission

--- server/authz/permission.go
+++ server/authz/permission.go
@@ -1,5 +1,16 @@
 package authz
 
+type permission string
+
+const (
+	PermissionCreateAgency     permission = "create_agency"
+	PermissionCreatePage       permission = "create_page"
+	PermissionProvisionDevice  permission = "provision_device"
+	PermissionActivateDevice   permission = "activate"
+	PermissionDeactivateDevice permission = "deactivate"
+	PermissionInviteUser       permission = "invite_user"
+)
+
 type Permission struct {
 	Relationship string
 	Resource     Resource
