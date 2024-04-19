41fa62e69956b7ef9df31fce9fdc44df5601a5c8
Jacob Smith <jsmithdenverdev@gmail.com>
2024-04-19 15:04:16 -0600

    fix messages

--- server/pubsub/message.go
+++ server/pubsub/message.go
@@ -1,31 +1,52 @@
 package pubsub
 
 import (
+	"database/sql/driver"
 	"encoding/json"
+	"errors"
 
 	"github.com/jsmithdenverdev/pager/models"
 )
 
-type Message struct {
-	models.Auditable
-	Topic   Topic  `json:"topic" db:"topic"`
-	payload []byte `json:"payload" db:"payload"`
-	Retries int    `json:"retries" db:"retries"`
+type Payload map[string]interface{}
+
+func (p Payload) Value() (driver.Value, error) {
+	return json.Marshal(p)
 }
 
-func NewMessage[P any](topic Topic, payload P) (Message, error) {
+func (p *Payload) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	return json.Unmarshal(b, &p)
+}
+
+type Payloader interface {
+	Payload() Payload
+}
+
+type Message struct {
+	models.Auditable
+	Topic   Topic   `json:"topic" db:"topic"`
+	Payload Payload `json:"payload" db:"payload"`
+	Retries int     `json:"retries" db:"retries"`
+}
+
+func NewMessage[P Payloader](topic Topic, payloader P) (Message, error) {
 	var message Message
 	message.Topic = topic
-	payloadB, err := json.Marshal(payload)
-	if err != nil {
-		return message, err
-	}
-	message.payload = payloadB
+	message.Payload = payloader.Payload()
 	return message, nil
 }
 
 func Unmarshal[T any](message Message, out *T) error {
-	if err := json.Unmarshal(message.payload, out); err != nil {
+	b, err := json.Marshal(message.Payload)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(b, out); err != nil {
 		return err
 	}
 	return nil
@@ -37,6 +58,20 @@ type PayloadProvisionUser struct {
 	Role     models.Role `json:"role"`
 }
 
+func (p PayloadProvisionUser) Payload() Payload {
+	var payload Payload = make(Payload)
+	payload["agencyId"] = p.AgencyID
+	payload["email"] = p.Email
+	payload["role"] = p.Role
+	return payload
+}
+
 type PayloadSendPage struct {
 	PageDeliveryID string `json:"pageDeliveryId"`
+}
+
+func (p PayloadSendPage) Payload() Payload {
+	var payload Payload = make(Payload)
+	payload["pageDeliveryId"] = p.PageDeliveryID
+	return payload
 }
