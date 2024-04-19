86962357d5529f74a63231631bff1b519dfc420e
Jacob Smith <jsmithdenverdev@gmail.com>
2024-04-19 09:33:06 -0600

    add page title

--- server/models/page.go
+++ server/models/page.go
@@ -6,5 +6,6 @@ package models
 type Page struct {
 	Auditable
 	AgencyID string `json:"agencyId" db:"agency_id"`
+	Title    string `json:"title" db:"title"`
 	Content  string `json:"content" db:"content"`
 }
