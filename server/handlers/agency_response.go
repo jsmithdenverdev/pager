022fcd71652088ebde010a1872b7b0ba8d0a099d
Jacob Smith <jsmithdenverdev@gmail.com>
2024-09-13 14:07:30 -0600

    Re-organize handler package. Add problem details

--- server/handlers/agency_response.go
+++ server/handlers/agency_response.go
@@ -2,11 +2,12 @@ package handlers
 
 import "github.com/jsmithdenverdev/pager/models"
 
-// agencyResponse is the response model representation of an Agency.
+// agencyResponse is the response model representation of a models.Agency.
 type agencyResponse struct {
 	Name string `json:"name"`
 }
 
+// toCreateAgencyResponse converts a models.Agency to a createAgencyResponse
 func toAgencyResponse(m models.Agency) agencyResponse {
 	return agencyResponse{
 		Name: m.Name,
