9f928245a8604a2afd079c00d4c866799cfc64de
Jacob Smith <jsmithdenverdev@gmail.com>
2024-09-15 20:41:36 -0600

    test

--- services/agency/build.sh
+++ services/agency/build.sh
@@ -1,49 +1,51 @@
 #!/bin/bash
 
-# Set the directory where the executables are located
-CMD_DIR="./cmd"
-DIST_DIR="./dist"
+echo "test"
 
-# Create the dist directory if it doesn't exist
-mkdir -p $DIST_DIR
+# # Set the directory where the executables are located
+# CMD_DIR="./cmd"
+# DIST_DIR="./dist"
 
-# Function to build and zip an executable
-build_and_zip_executable() {
-  EXECUTABLE=$1
+# # Create the dist directory if it doesn't exist
+# mkdir -p $DIST_DIR
 
-  # Create the output directory for the executable
-  OUTPUT_DIR="$DIST_DIR/$EXECUTABLE"
-  mkdir -p $OUTPUT_DIR
+# # Function to build and zip an executable
+# build_and_zip_executable() {
+#   EXECUTABLE=$1
 
-  # Build the executable and output to the dist directory as "handler"
-  echo "Building $EXECUTABLE..."
-  go build -o "$OUTPUT_DIR/handler" "$CMD_DIR/$EXECUTABLE"
+#   # Create the output directory for the executable
+#   OUTPUT_DIR="$DIST_DIR/$EXECUTABLE"
+#   mkdir -p $OUTPUT_DIR
 
-  # Check if the build was successful
-  if [ $? -ne 0 ]; then
-    echo "Failed to build $EXECUTABLE"
-    exit 1
-  fi
+#   # Build the executable and output to the dist directory as "handler"
+#   echo "Building $EXECUTABLE..."
+#   go build -o "$OUTPUT_DIR/handler" "$CMD_DIR/$EXECUTABLE"
 
-  # Zip the handler into the dist directory
-  echo "Zipping $EXECUTABLE..."
-  (cd $OUTPUT_DIR && zip -r "../$EXECUTABLE.zip" handler)
+#   # Check if the build was successful
+#   if [ $? -ne 0 ]; then
+#     echo "Failed to build $EXECUTABLE"
+#     exit 1
+#   fi
 
-  # Check if the zip was successful
-  if [ $? -ne 0 ]; then
-    echo "Failed to zip $EXECUTABLE"
-    exit 1
-  fi
+#   # Zip the handler into the dist directory
+#   echo "Zipping $EXECUTABLE..."
+#   (cd $OUTPUT_DIR && zip -r "../$EXECUTABLE.zip" handler)
 
-  echo "$EXECUTABLE has been successfully built and zipped."
-}
+#   # Check if the zip was successful
+#   if [ $? -ne 0 ]; then
+#     echo "Failed to zip $EXECUTABLE"
+#     exit 1
+#   fi
 
-# Array of executable names
-executables=("create_agency")
+#   echo "$EXECUTABLE has been successfully built and zipped."
+# }
 
-# Loop over each executable and call the build_and_zip_executable function
-for EXECUTABLE in "${executables[@]}"; do
-  build_and_zip_executable "$EXECUTABLE"
-done
+# # Array of executable names
+# executables=("create_agency")
 
-echo "All executables built and zipped successfully."
+# # Loop over each executable and call the build_and_zip_executable function
+# for EXECUTABLE in "${executables[@]}"; do
+#   build_and_zip_executable "$EXECUTABLE"
+# done
+
+# echo "All executables built and zipped successfully."
