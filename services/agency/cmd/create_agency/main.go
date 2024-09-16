2211906660abb222965e7f771dd0a274f15ccc12
Jacob Smith <jsmithdenverdev@gmail.com>
2024-09-15 20:32:40 -0600

    stub out agency service

---
+++ services/agency/cmd/create_agency/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/jsmithdenverdev/pager/services/agency/internal/handlers"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "run failed: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	lambda.StartWithOptions(handlers.CreateAgency())
+	return nil
+}
