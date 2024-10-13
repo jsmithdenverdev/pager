747905918a9b99fac5a2cc932dde40cc210ffd13
Jacob Smith <jsmithdenverdev@gmail.com>
2024-10-13 14:39:38 -0600

    add problem details (#56)
    * add problem details
    
    * fix ci ref
    
    * dirnames filter and go sum updates
    
    * specify path for changed files
    
    * change relative dir to pkg
    
    * correct ci and cd builds

---
+++ pkg/problemdetails/problem.go
@@ -0,0 +1,7 @@
+package problemdetails
+
+// Problem represents an issue found during validation.
+type Problem struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
