0fe264cb52243c76ac05d77ce8e5bd4f23c3898d
Jacob Smith <jsmithdenverdev@gmail.com>
2024-10-21 15:34:44 -0600

    add problem detail support to valid package

--- pkg/valid/go.mod
+++ pkg/valid/go.mod
@@ -2,6 +2,6 @@ module github.com/jsmithdenverdev/pager/pkg/valid
 
 go 1.23.2
 
-require github.com/jsmithdenverdev/pager/pkg/problemdetails v1.2.0
+require github.com/jsmithdenverdev/pager/pkg/problemdetail v1.1.0
 
 require github.com/aws/aws-lambda-go v1.47.0 // indirect
---
+++ pkg/valid/problemdetail.go
@@ -0,0 +1,26 @@
+package valid
+
+import (
+	"net/http"
+
+	"github.com/jsmithdenverdev/pager/pkg/problemdetail"
+)
+
+type ProblemDetail struct {
+	*problemdetail.ProblemDetail
+	Problems []Problem `json:"problems"`
+}
+
+func NewProblemDetail(problems []Problem) problemdetail.ProblemDetailer {
+	pd := problemdetail.New(
+		"validation-error",
+		problemdetail.WithTitle("Invalid request"),
+		problemdetail.WithDetail("The request you supplied didn't pass validation."))
+
+	pd.WriteStatus(http.StatusBadRequest)
+
+	return &ProblemDetail{
+		ProblemDetail: pd,
+		Problems:      problems,
+	}
+}
