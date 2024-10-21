0ea6d5d55ae0d1145e679e725ee9dd378369f185
Jacob Smith <jsmithdenverdev@gmail.com>
2024-10-21 10:53:57 -0600

    problem detail package

--- pkg/problemdetails/problem.go
+++
@@ -1,7 +0,0 @@
-package problemdetails
-
-// Problem represents an issue found during validation.
-type Problem struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-}
