53c4e123e8cc9c5847c8d007e431f838d248cd14
Jacob Smith <jsmithdenverdev@gmail.com>
2024-10-22 14:37:48 -0600

    fix struct tags for userinfo

--- pkg/authz/user_info.go
+++ pkg/authz/user_info.go
@@ -3,9 +3,9 @@ package authz
 // UserInfo represents information about a user needed to make authorization
 // decisions.
 type UserInfo struct {
-	Email        string        `json:"emai"`
+	Email        string        `json:"email"`
 	Entitlements []Entitlement `json:"entitlements"`
-	IPDID        string        `json:"ipdId"`
+	IPDID        string        `json:"idpId"`
 	Status       string        `json:"status"`
 	Accounts     map[string]struct {
 		Roles []string `json:"roles"`
