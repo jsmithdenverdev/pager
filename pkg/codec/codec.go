809b5fd607662419f66304e5563b904c5f1845ba
Jacob Smith <jsmithdenverdev@gmail.com>
2024-10-22 11:10:01 -0600

    separate interfaces and func types

--- pkg/codec/codec.go
+++ pkg/codec/codec.go
@@ -8,6 +8,22 @@ import (
 
 type EncoderOption[TOut any] func(*TOut)
 
-type Encoder[TIn, TOut any] func(ctx context.Context, in TIn, opts ...EncoderOption[TOut]) (TOut, error)
+type Encoder[TIn, TOut any] interface {
+	Encode(ctx context.Context, in TIn, opts ...EncoderOption[TOut]) (TOut, error)
+}
 
-type Decoder[TIn any, TOut valid.Validator] func(ctx context.Context, in TIn) (TOut, error)
+type Decoder[TIn any, TOut valid.Validator] interface {
+	Decode(ctx context.Context, in TIn) (TOut, error)
+}
+
+type EncoderFunc[TIn, TOut any] func(ctx context.Context, in TIn, opts ...EncoderOption[TOut]) (TOut, error)
+
+func (fn EncoderFunc[TIn, TOut]) Encode(ctx context.Context, in TIn, opts ...EncoderOption[TOut]) (TOut, error) {
+	return fn(ctx, in, opts...)
+}
+
+type DecoderFunc[TIn any, TOut valid.Validator] func(ctx context.Context, in TIn) (TOut, error)
+
+func (fn DecoderFunc[TIn, TOut]) Decode(ctx context.Context, in TIn) (TOut, error) {
+	return fn(ctx, in)
+}
