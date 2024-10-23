c767c63194dbfcb383aa0afa8601e6b96dfc5ca0
Jacob Smith <jsmithdenverdev@gmail.com>
2024-10-23 14:15:54 -0600

    add funtionality for storing userinfo in context

--- pkg/authz/context.go
+++ pkg/authz/context.go
@@ -1,11 +1,14 @@
 package authz
 
-import "context"
+import (
+	"context"
+)
 
 type contextKey string
 
 const (
-	contextKeyClient contextKey = "client"
+	contextKeyClient   contextKey = "client"
+	contextKeyUserInfo contextKey = "user"
 )
 
 func AddClientToContext(ctx context.Context, authorizer Authorizer) context.Context {
@@ -15,4 +18,13 @@ func AddClientToContext(ctx context.Context, authorizer Authorizer) context.Cont
 func RetrieveClientFromContext(ctx context.Context) (Authorizer, bool) {
 	client, ok := ctx.Value(contextKeyClient).(*client)
 	return client, ok
+}
+
+func AddUserInfoToContext(ctx context.Context, userInfo UserInfo) context.Context {
+	return context.WithValue(ctx, contextKeyUserInfo, userInfo)
+}
+
+func RetrieveUserInfoFromContext(ctx context.Context) (UserInfo, bool) {
+	userInfo, ok := ctx.Value(contextKeyUserInfo).(UserInfo)
+	return userInfo, ok
 }
