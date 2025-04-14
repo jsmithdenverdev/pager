e78cddce786a987e9edfba2379447d8613a35f91
Jacob Smith <jsmithdenverdev@gmail.com>
2025-04-14 10:32:50 -0600

    Add documentation and rename userInfo field to user in authz package (#124)

--- pkg/authz/client.go
+++ pkg/authz/client.go
@@ -8,14 +8,18 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 )
 
+// optionFunc is a function type that modifies a Client.
 type optionFunc func(*Client)
 
+// Client represents a client for interacting with the authorization service.
+// It holds user information, a policy store ID, and an AWS Verified Permissions client.
 type Client struct {
-	userInfo      User
+	user          User
 	policyStoreId string
 	*verifiedpermissions.Client
 }
 
+// NewClient creates a new Client instance, applying any provided options.
 func NewClient(options ...optionFunc) *Client {
 	c := new(Client)
 	for _, o := range options {
@@ -24,16 +28,19 @@ func NewClient(options ...optionFunc) *Client {
 	return c
 }
 
+// IsAuthorizedInput represents the input required to check if an action is authorized.
 type IsAuthorizedInput struct {
 	Resource *types.EntityIdentifier
 	Action   *types.ActionIdentifier
 	Entities []types.EntityItem
 }
 
+// IsAuthorized checks if the specified action is authorized for the given entities.
+// It encodes user entitlements and creates entity definitions for the authorization request.
 func (c *Client) IsAuthorized(ctx context.Context, input IsAuthorizedInput) (bool, error) {
 	// Encode the users entitlements into a slice of string attribute values
 	var entitlementAttributeValues []types.AttributeValue
-	for _, entitlement := range c.userInfo.Entitlements {
+	for _, entitlement := range c.user.Entitlements {
 		entitlementAttributeValues = append(entitlementAttributeValues, &types.AttributeValueMemberString{
 			Value: string(entitlement),
 		})
@@ -47,7 +54,7 @@ func (c *Client) IsAuthorized(ctx context.Context, input IsAuthorizedInput) (boo
 			{
 				Identifier: &types.EntityIdentifier{
 					EntityType: aws.String("pager::User"),
-					EntityId:   aws.String(c.userInfo.IPDID),
+					EntityId:   aws.String(c.user.IPDID),
 				},
 				Attributes: map[string]types.AttributeValue{
 					"entitlements": &types.AttributeValueMemberSet{
@@ -60,10 +67,10 @@ func (c *Client) IsAuthorized(ctx context.Context, input IsAuthorizedInput) (boo
 
 	// If the user is a member of agencies add those agencies to the auth request
 	// context.
-	if len(c.userInfo.Agencies) > 0 {
+	if len(c.user.Agencies) > 0 {
 		// Encode the users agencies into a slice of entity identifier attribute value
 		var agencyAttributeValues []types.AttributeValue
-		for agency := range c.userInfo.Agencies {
+		for agency := range c.user.Agencies {
 			agencyAttributeValues = append(agencyAttributeValues, &types.AttributeValueMemberEntityIdentifier{
 				Value: types.EntityIdentifier{
 					EntityType: aws.String("pager::Agency"),
@@ -79,21 +86,21 @@ func (c *Client) IsAuthorized(ctx context.Context, input IsAuthorizedInput) (boo
 
 	// If the user is making a request for a specific agency add the agency to
 	// the auth request context.
-	if len(c.userInfo.ActiveAgency) > 0 {
+	if len(c.user.ActiveAgency) > 0 {
 		// entityDefinitions.Value[0] is the user
 		entityDefinitions.Value[0].Attributes["currentAgency"] = &types.AttributeValueMemberEntityIdentifier{
 			Value: types.EntityIdentifier{
 				EntityType: aws.String("pager::Agency"),
-				EntityId:   aws.String(c.userInfo.ActiveAgency),
+				EntityId:   aws.String(c.user.ActiveAgency),
 			},
 		}
-		if agency, ok := c.userInfo.Agencies[c.userInfo.ActiveAgency]; ok {
+		if agency, ok := c.user.Agencies[c.user.ActiveAgency]; ok {
 			entityDefinitions.Value = append(
 				entityDefinitions.Value,
 				types.EntityItem{
 					Identifier: &types.EntityIdentifier{
 						EntityType: aws.String("pager::Agency"),
-						EntityId:   aws.String(c.userInfo.ActiveAgency),
+						EntityId:   aws.String(c.user.ActiveAgency),
 					},
 					Attributes: map[string]types.AttributeValue{
 						"membership": &types.AttributeValueMemberEntityIdentifier{
@@ -115,7 +122,7 @@ func (c *Client) IsAuthorized(ctx context.Context, input IsAuthorizedInput) (boo
 		PolicyStoreId: aws.String(c.policyStoreId),
 		Principal: &types.EntityIdentifier{
 			EntityType: aws.String("pager::User"),
-			EntityId:   aws.String(c.userInfo.IPDID),
+			EntityId:   aws.String(c.user.IPDID),
 		},
 		Resource: input.Resource,
 		Action:   input.Action,
@@ -129,19 +136,21 @@ func (c *Client) IsAuthorized(ctx context.Context, input IsAuthorizedInput) (boo
 	return result.Decision == types.DecisionAllow, nil
 }
 
+// WithVerifiedPermissionsClient returns an option that sets the Verified Permissions client.
 func WithVerifiedPermissionsClient(vpc *verifiedpermissions.Client) optionFunc {
 	return func(c *Client) {
 		c.Client = vpc
 	}
 }
 
+// WithUserInfo returns an option that sets the user information.
 func WithUserInfo(userInfo User) optionFunc {
 	return func(c *Client) {
-		c.userInfo = userInfo
-		return
+		c.user = userInfo
 	}
 }
 
+// WithPolicyStoreID returns an option that sets the policy store ID.
 func WithPolicyStoreID(policyStoreId string) optionFunc {
 	return func(c *Client) {
 		c.policyStoreId = policyStoreId
--- pkg/authz/context.go
+++ pkg/authz/context.go
@@ -1,3 +1,4 @@
+// Package authz provides context utilities for authorization.
 package authz
 
 import (
@@ -7,23 +8,29 @@ import (
 type contextKey string
 
 const (
-	contextKeyClient   contextKey = "client"
+	// contextKeyClient is the key used to store the Client in a context.
+	contextKeyClient contextKey = "client"
+	// contextKeyUserInfo is the key used to store the User information in a context.
 	contextKeyUserInfo contextKey = "user"
 )
 
+// WithClient returns a new context with the provided Client stored in it.
 func WithClient(ctx context.Context, authorizer *Client) context.Context {
 	return context.WithValue(ctx, contextKeyClient, authorizer)
 }
 
+// ClientFrom extracts the Client from the context, if present.
 func ClientFrom(ctx context.Context) (*Client, bool) {
 	client, ok := ctx.Value(contextKeyClient).(*Client)
 	return client, ok
 }
 
+// WithUser returns a new context with the provided User information stored in it.
 func WithUser(ctx context.Context, userInfo User) context.Context {
 	return context.WithValue(ctx, contextKeyUserInfo, userInfo)
 }
 
+// UserFrom extracts the User information from the context, if present.
 func UserFrom(ctx context.Context) (User, bool) {
 	userInfo, ok := ctx.Value(contextKeyUserInfo).(User)
 	return userInfo, ok
