01861deb580a5a2642cd3554da9f687c6612f9b7
Jake Smith <jsmithdenverdev@gmail.com>
2025-04-22 13:03:47 -0600

    Add endpoint and page service mains

---
+++ services/endpoint/cmd/app/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		fmt.Fprintf(os.Stderr, "run failed: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	return nil
+}
