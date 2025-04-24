55517d55f21196d48312dc99c419435bb2a50fef
Jake Smith <jsmithdenverdev@gmail.com>
2025-04-24 10:24:06 -0600

    Add page service with API Gateway integration and Lambda handler

--- services/page/cmd/app/main.go
+++ services/page/cmd/app/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
@@ -14,5 +18,17 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	loghandler := slog.NewJSONHandler(os.Stdout, nil)
+	lambda.Start(handler(loghandler))
 	return nil
+}
+
+func handler(loghandler slog.Handler) func(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return func(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		logger := slog.New(loghandler)
+		logger.InfoContext(ctx, "request received", "event", event)
+		return events.APIGatewayProxyResponse{
+			StatusCode: 200,
+		}, nil
+	}
 }
