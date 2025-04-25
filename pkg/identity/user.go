a74a1cd9ae5b124ecd79a10585815407195f8a52
Jacob Smith <jsmithdenverdev@gmail.com>
2025-04-25 16:16:34 -0600

    Add membership struct

--- pkg/identity/user.go
+++ pkg/identity/user.go
@@ -3,6 +3,14 @@ package identity
 // User represents a user. This representation is used for authorization
 // decisions.
 type User struct {
-	Email string `json:"email"`
-	IPDID string `json:"idpId"`
+	Email       string                `json:"email"`
+	IPDID       string                `json:"idpId"`
+	Memberships map[string]Membership `json:"memberships"`
+}
+
+// Membership represents a binding between a user and an Agency.
+type Membership struct {
+	AgencyID string `json:"agencyId"`
+	Name     string `json:"name"`
+	Role     string `json:"role"`
 }
