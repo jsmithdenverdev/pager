c9af1c99a9b4c494d9a3727ca79bf9df402f90b1
Jake Smith <jsmithdenverdev@gmail.com>
2025-04-28 14:07:26 -0600

    Refactor agency service handlers, add membership list endpoints, add agency read

---
+++ services/agency/internal/app/handlers.go
@@ -0,0 +1,198 @@
+package app
+
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/jsmithdenverdev/pager/pkg/identity"
+)
+
+// listMyMemberships returns a list of agencies the calling user is a member of.
+func listMyMemberships(config Config, logger *slog.Logger, client *dynamodb.Client) http.Handler {
+	type listMyMembershipsResponse struct {
+		Memberships []membershipResponse `json:"agencies"`
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idpid := r.Header.Get("x-pager-userid")
+
+		result, err := client.Query(r.Context(), &dynamodb.QueryInput{
+			TableName:              &config.AgencyTableName,
+			KeyConditionExpression: aws.String("pk = :idpid"),
+			ExpressionAttributeValues: map[string]types.AttributeValue{
+				":idpid": &types.AttributeValueMemberS{
+					Value: fmt.Sprintf("idpid#%s", idpid),
+				},
+			},
+		})
+		if err != nil {
+			logger.ErrorContext(r.Context(), "failed to query agencies", slog.Any("error", err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		var memberships []membership
+		if result.Items != nil {
+			for _, item := range result.Items {
+				var membership membership
+				if err := attributevalue.UnmarshalMap(item, &membership); err != nil {
+					logger.ErrorContext(r.Context(), "failed to unmarshal membership record", slog.Any("error", err))
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				memberships = append(memberships, membership)
+			}
+		}
+
+		response := new(listMyMembershipsResponse)
+
+		for _, membership := range memberships {
+			response.Memberships = append(response.Memberships, membershipResponse{
+				AgencyID: strings.Split(membership.SK, "#")[1],
+				UserID:   strings.Split(membership.PK, "#")[1],
+				Role:     membership.Role,
+			})
+		}
+
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			logger.ErrorContext(r.Context(), "failed to encode response", slog.Any("error", err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+// listAgencyMemberships returns a list of memberships in the specified agency.
+// The calling user must have a membership in the specified agency.
+func listAgencyMemberships(config Config, logger *slog.Logger, client *dynamodb.Client) http.Handler {
+	type listAgencyMembershipsResponse struct {
+		Memberships []membershipResponse `json:"memberships"`
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var (
+			user        identity.User
+			userinfostr = r.Header.Get("x-pager-userinfo")
+			agencyid    = r.PathValue("id")
+		)
+
+		if err := json.Unmarshal([]byte(userinfostr), &user); err != nil {
+			logger.ErrorContext(r.Context(), "failed to unmarshal user info", slog.Any("error", err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if _, ok := user.Memberships[agencyid]; !ok {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		result, err := client.Query(r.Context(), &dynamodb.QueryInput{
+			TableName:              &config.AgencyTableName,
+			KeyConditionExpression: aws.String("pk = :agencyid"),
+			ExpressionAttributeValues: map[string]types.AttributeValue{
+				":agencyid": &types.AttributeValueMemberS{
+					Value: fmt.Sprintf("agency#%s", agencyid),
+				},
+			},
+		})
+
+		if err != nil {
+			logger.ErrorContext(r.Context(), "failed to query agencies", slog.Any("error", err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		var memberships []membership
+		if result.Items != nil {
+			for _, item := range result.Items {
+				var membership membership
+				if err := attributevalue.UnmarshalMap(item, &membership); err != nil {
+					logger.ErrorContext(r.Context(), "failed to unmarshal membership record", slog.Any("error", err))
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				memberships = append(memberships, membership)
+			}
+		}
+
+		response := new(listAgencyMembershipsResponse)
+
+		for _, membership := range memberships {
+			response.Memberships = append(response.Memberships, membershipResponse{
+				AgencyID: strings.Split(membership.PK, "#")[1],
+				UserID:   strings.Split(membership.SK, "#")[1],
+				Role:     membership.Role,
+			})
+		}
+
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			logger.ErrorContext(r.Context(), "failed to encode response", slog.Any("error", err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+// readAgency returns a single agency by ID.
+// The calling user must have a membership in the specified agency.
+func readAgency(config Config, logger *slog.Logger, client *dynamodb.Client) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var (
+			user        identity.User
+			userinfostr = r.Header.Get("x-pager-userinfo")
+			agencyid    = r.PathValue("id")
+		)
+
+		if err := json.Unmarshal([]byte(userinfostr), &user); err != nil {
+			logger.ErrorContext(r.Context(), "failed to unmarshal user info", slog.Any("error", err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if _, ok := user.Memberships[agencyid]; !ok {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		result, err := client.GetItem(r.Context(), &dynamodb.GetItemInput{
+			TableName: &config.AgencyTableName,
+			Key: map[string]types.AttributeValue{
+				"pk": &types.AttributeValueMemberS{
+					Value: fmt.Sprintf("agency#%s", agencyid),
+				},
+				"sk": &types.AttributeValueMemberS{
+					Value: fmt.Sprintf("agency#%s", agencyid),
+				},
+			},
+		})
+
+		if err != nil {
+			logger.ErrorContext(r.Context(), "failed to get agency", slog.Any("error", err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		var agency agency
+		if result.Item != nil {
+			if err := attributevalue.UnmarshalMap(result.Item, &agency); err != nil {
+				logger.ErrorContext(r.Context(), "failed to unmarshal agency record", slog.Any("error", err))
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		}
+
+		if err := json.NewEncoder(w).Encode(toAgencyResponse(agency)); err != nil {
+			logger.ErrorContext(r.Context(), "failed to encode response", slog.Any("error", err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
--- services/agency/internal/app/routes.go
+++ services/agency/internal/app/routes.go
@@ -9,6 +9,7 @@ import (
 )
 
 func addRoutes(mux *http.ServeMux, config Config, logger *slog.Logger, client *dynamodb.Client) {
-	mux.Handle(fmt.Sprintf("GET /%s", config.Environment), listMemberships(config, logger, client))
-	mux.Handle(fmt.Sprintf("GET /%s/{id}", config.Environment), readAgencyById(logger, client))
+	mux.Handle(fmt.Sprintf("GET /%s", config.Environment), listMyMemberships(config, logger, client))
+	mux.Handle(fmt.Sprintf("GET /%s/{id}", config.Environment), readAgency(config, logger, client))
+	mux.Handle(fmt.Sprintf("GET /%s/{id}/memberships", config.Environment), listAgencyMemberships(config, logger, client))
 }
