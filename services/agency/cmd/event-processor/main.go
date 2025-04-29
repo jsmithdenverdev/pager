a6c39602e1c299e8789394663e4985c197ba3398
Jake Smith <jsmithdenverdev@gmail.com>
2025-04-29 15:33:56 -0600

    Add event processor lambda and SQS queue for handling user events

---
+++ services/agency/cmd/event-processor/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	if err := run(context.Background(), os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "run failed: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, stdout io.Writer) error {
+	logger := slog.New(slog.NewJSONHandler(stdout, nil))
+	lambda.Start(handler(logger))
+	return nil
+}
+
+func handler(logger *slog.Logger) func(ctx context.Context, event events.SQSEvent) (events.SQSEventResponse, error) {
+	return func(ctx context.Context, event events.SQSEvent) (events.SQSEventResponse, error) {
+		logger.InfoContext(ctx, "user event processor", slog.Any("event", event))
+		return events.SQSEventResponse{}, nil
+	}
+}
