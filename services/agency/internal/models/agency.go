4eff0148e7acdae50a8eb885f6783b9978105dfc
Jake Smith <jsmithdenverdev@gmail.com>
2025-05-29 11:04:38 -0600

    undoing service changes

--- services/agency/internal/models/agency.go
+++ services/agency/internal/models/agency.go
@@ -1,10 +1,6 @@
 package models
 
-import (
-	"fmt"
-	"strings"
-	"time"
-)
+import "time"
 
 type AgencyStatus = string
 
@@ -15,31 +11,13 @@ const (
 
 // agency represents an agency in the database.
 type Agency struct {
-	ID         string       `dynamodbav:"id"`
+	PK         string       `dynamodbav:"pk"`
+	SK         string       `dynamodbav:"sk"`
+	Type       EntityType   `dynamodbav:"type"`
 	Name       string       `dynamodbav:"name"`
 	Status     AgencyStatus `dynamodbav:"status"`
 	Created    time.Time    `dynamodbav:"created"`
 	Modified   time.Time    `dynamodbav:"modified"`
 	CreatedBy  string       `dynamodbav:"createdBy"`
 	ModifiedBy string       `dynamodbav:"modifiedBy"`
-}
-
-func (a Agency) Type() string {
-	return EntityTypeAgency
-}
-
-func (a Agency) EncodeKey() attributevalue.Key {
-	return attributevalue.Key{
-		PK: fmt.Sprintf("agency#%s", a.ID),
-		SK: "meta",
-	}
-}
-
-func (a Agency) DecodeKey(key attributevalue.Key) error {
-	pkSplit := strings.Split(key.PK, "#")
-	if len(pkSplit) != 2 {
-		return fmt.Errorf("invalid PK: %s", key.PK)
-	}
-	a.ID = pkSplit[1]
-	return nil
 }
